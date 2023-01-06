a268dc4aaf50f540d1d029ee4f148330b044821a
muhwahyuagengpambudi <agengw825@gmail.com>
2023-01-06 10:03:38 +0700

    first commit

---
+++ assets/main.js
@@ -0,0 +1,91 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show');
+    });
+  }
+};
+showMenu('nav-toggle', 'nav-menu');
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu');
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show');
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active');
+    } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2000,
+  delay: 200,
+  //     reset: true
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', { delay: 400 });
+sr.reveal('.home__social-icon', { interval: 200 });
+sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 });
+
+// funchion progress bar and to top
+
+const showOnPx = 100;
+const backToTopButton = document.querySelector('.back-to-top');
+const pageProgressBar = document.querySelector('.progress-bar');
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+const goToTop = () => {
+  document.body.scrollIntoView({
+    behavior: 'smooth',
+  });
+};
+
+document.addEventListener('scroll', () => {
+  console.log('Scroll Height: ', scrollContainer().scrollHeight);
+  console.log('Client Height: ', scrollContainer().clientHeight);
+
+  const scrolledPercentage = (scrollContainer().scrollTop / (scrollContainer().scrollHeight - scrollContainer().clientHeight)) * 100;
+
+  pageProgressBar.style.width = `${scrolledPercentage}%`;
+
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove('hidden-top');
+  } else {
+    backToTopButton.classList.add('hidden-top');
+  }
+});
+
+backToTopButton.addEventListener('click', goToTop);
+
+// end of function progress bar and to top
